3b814040e79a175b79d2922080ea0af271e4fd7f
VladimirOrlov <Vladimir.Orlov.A@mail.ru>
2020-02-06 21:58:25 +0600

    Добавьте файлы проекта.

---
+++ Lesson_WPF_pract_20200206/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace Lesson_WPF_pract_20200206
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGen_Click(object sender, RoutedEventArgs e)
+        {
+            string text = tbxText.Text;
+            int size = 0;
+
+            foreach(RadioButton rb in spRbList.Children)
+            {
+                if ((bool)rb.IsChecked)
+                {
+                    size = Convert.ToInt32(rb.Content);
+                    break;
+                }   
+            }
+
+            QrApp.lib.Qr.genQR(text, size);
+        }
+    }
+}
---
+++ QrApp.lib/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RestSharp;
+using QrApp.lib;
+
+namespace QrApp.lib
+{
+    public class Qr
+    {
+        public static void genQR(string text, int size)
+        {
+
+            var client = new RestClient("https://qrcoder.ru");
+            var request = new RestRequest($"code/?{text}&{size}&0");
+
+            var response = client.Execute(request);
+            var result = response.Content;
+        }
+    }
+}
